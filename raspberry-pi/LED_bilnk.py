7b78848f05bb9081509a18a8af6391a2794f28e6
Bigdaddyt1 <tbrown08@charlottesvilleschools.org>
2023-09-07 13:39:12 -0400

    fghj

---
+++ raspberry-pi/LED_bilnk.py
@@ -0,0 +1,10 @@
+import board
+import digitalio
+import time
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+while True:
+    led.value = True
+    time.sleep(.5)
+    led.value = False
+    time.sleep(.5)
